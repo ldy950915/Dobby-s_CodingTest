1c6833356465f28c1b2ebf5966ef82f6e6cbbf28
hi <hi@DESKTOP-UALNEVN>
2022-05-29 23:57:32 +0900

    재귀함수

---
+++ inflearn/src/DFS_BFS/recursiveFunction_1.java
@@ -0,0 +1,16 @@
+package DFS_BFS;
+
+//Àç±ÍÇÔ¼ö - ±íÀÌ ¿ì¼± Å½»ö
+public class recursiveFunction_1 {
+
+	public void DFS(int n) {
+		
+	}
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		recursiveFunction_1 R = new recursiveFunction_1();
+		R.DFS(3);
+	}
+
+}
