0327415eac8096d9d3d039f7cc27b66906b4326b
hi <hi@DESKTOP-UALNEVN>
2022-05-30 00:39:29 +0900

    재귀함수 기초

--- inflearn/src/DFS_BFS/recursiveFunction_1.java
+++ inflearn/src/DFS_BFS/recursiveFunction_1.java
@@ -2,9 +2,14 @@ package DFS_BFS;
 
 //재귀함수 - 깊이 우선 탐색
 public class recursiveFunction_1 {
-
+	//무한적으로 작동되므로 if~else로 멈추게 하기!
 	public void DFS(int n) {
-		
+		if(n==0) {
+			return;//void에서는 종료를 뜻함.
+		}else {
+			DFS(n-1);
+			System.out.print(n + " ");
+		}
 	}
 	
 	public static void main(String[] args) {
