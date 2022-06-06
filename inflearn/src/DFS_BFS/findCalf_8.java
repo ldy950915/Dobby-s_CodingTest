58c971aa6ccc34f5929e86216e298384aaee87ef
hi <hi@DESKTOP-UALNEVN>
2022-06-06 18:02:20 +0900

    송아지찾기(BFS)

--- inflearn/src/DFS_BFS/findCalf_8.java
+++ inflearn/src/DFS_BFS/findCalf_8.java
@@ -7,18 +7,19 @@ import java.util.Scanner;
 //송아지 찾기
 public class findCalf_8 {
 	int answer = 0;//최소 횟수
-	int[] dis = {1,-1,5};//움직임
+	int[] dis = {1,-1,5};//이동
 	int[] ch;//방문 체크
 	Queue<Integer> Q = new LinkedList<Integer>(); //정수 저장 
 	public int BFS(int s, int e) {
 		ch = new int[10001]; //문제에서 index크기가 10000이라 명명 되어있음.
-		ch[s] = 1; //출발지점
-		Q.offer(s);
-		int L = 0;//루트 노드
-		while(!Q.isEmpty()) {
+		ch[s] = 1; //출발지점은 1~10000까지
+		Q.offer(s);	//Q에 출발 지점 초기값 셋팅
+		int L = 0;//루트 노드 - 레벨은 0부터
+		while(!Q.isEmpty()) { //Q의 값이 있을때만 while문 돌기
 			int len = Q.size();//레벨에 있는 원소의 개수
 			for(int i =0; i<len; i++) {
 				int x = Q.poll();
+				System.out.println("x :" + x);
 				for(int j=0;j<3;j++) {
 					int nx = x+dis[j];
 					if(nx==e) return L+1; //부모노드 기준이라서 +1을 해줘야함
@@ -30,7 +31,7 @@ public class findCalf_8 {
 			}
 			L++;//레벨 증가
 		}
-		return 0;
+		return answer;
 	}
 	
 	public static void main(String[] args) {
