05acc3ce8ff9bc253f88965a40fad3aa8f6a6b81
hi <hi@DESKTOP-UALNEVN>
2022-07-02 13:54:05 +0900

    섬나라 아일랜드(DFS)

---
+++ inflearn/src/DFS_BFS_8/Island_13.java
@@ -0,0 +1,52 @@
+package DFS_BFS_8;
+
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Scanner;
+
+public class Island_13 {
+	static int[] dx = {-1,-1,0,1,1,1,0,-1};	//За
+	static int[] dy = {0,1,1,1,0,-1,-1,-1};	//ї­
+	
+	static int n,answer=0;
+	static int[][] dis;
+	static Queue<Point> Q = new LinkedList<Point>();
+	public void DFS(int x,int y, int[][] board){
+		for(int i=0;i<8;i++) {
+			int nx = x + dx[i];
+			int ny = y + dy[i];
+			if(nx>=0&&nx<n&&ny>=0&&ny<n && board[nx][ny]==1) {
+				board[nx][ny]=0;
+				DFS(nx,ny,board);
+			}
+		}
+		
+	}
+	public void solution(int[][] board) {
+		for(int i=0;i<n;i++) {
+			for(int j=0;j<n;j++) {
+				if(board[i][j] == 1) {
+					answer++;
+					board[i][j]=0;
+					DFS(i,j,board);
+				}
+			}
+		}
+	}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Island_13 I = new Island_13();
+		Scanner sc = new Scanner(System.in);
+		n = sc.nextInt();
+		int[][] board = new int[n][n];
+		dis = new int[n][n];
+		for(int i=0;i<n;i++) {
+			for(int j=0;j<n;j++) {
+				board[i][j] = sc.nextInt();
+			}
+		}
+		I.solution(board);
+		System.out.println(answer);
+	}
+
+}
