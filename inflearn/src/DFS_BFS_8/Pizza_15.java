9a25db84144c143bb32712d9b5c94ffa12d153a1
hi <hi@DESKTOP-UALNEVN>
2022-07-05 21:55:44 +0900

    피자배달

---
+++ inflearn/src/DFS_BFS_8/Pizza_15.java
@@ -0,0 +1,28 @@
+package DFS_BFS_8;
+
+import java.util.Scanner;
+
+//도시의 피자배달거리 = 각 집의 피자배달거리의 합
+public class Pizza_15 {
+	static int n,m;
+	static int[] dx = {-1,-1,0,1,1,1,0,-1};	//행
+	static int[] dy = {0,1,1,1,0,-1,-1,-1};	//열
+	static int[] home,pizza;
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Pizza_15 P = new Pizza_15();
+		Scanner sc = new Scanner(System.in);
+		n = sc.nextInt();
+		m = sc.nextInt();
+		
+		int[][] board = new int[n+1][n+1];
+		
+		for(int i=1;i<=n;i++) {
+			for(int j=1;j<=n;j++) {
+			
+			}
+		}
+		
+	}
+
+}
